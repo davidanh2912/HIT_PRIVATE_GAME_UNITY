07a59a84044b3db524a45fbec10ec3734a799eb3
DavidAnh <161005724+davidanh2912@users.noreply.github.com>
2024-10-28 18:16:47 +0700

    BTVN T2

---
+++ BTVN/BTVNT2/BTVN1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BTVN1;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double A = 9, omegaA = 5 * Math.PI, tA = 2.125, T = 2 * Math.PI / omegaA;
+        int t1 = (int)(tA / T); double tconLai = tA - t1 * T;
+        double quangDuong = t1 * 4 * A + 2 * A * Math.Abs(Math.Cos(omegaA * tconLai + Math.PI / 2));
+        Console.WriteLine("Quang duong vat A di chuyen duoc sau 2.125s la: " + quangDuong + "cm\n");
+        Console.WriteLine("phuong trinh dao dong B: x(t)= acos(bπt+π/2) (cm)");
+        Console.Write("\nnhap bien do cua vat dao dong B: ");
+        double B = double.Parse(Console.ReadLine());
+        Console.Write("nhap omega cua vat dao dong B = ?π: ");
+        double omegaB = double.Parse(Console.ReadLine()) * Math.PI;
+        Console.WriteLine("nhap thoi gian dao dong cua vat: ");
+        double tB = double.Parse(Console.ReadLine());
+        T = 2 * Math.PI / omegaB;
+        int t2 = (int)(tB / T); double tconlaiB = tB - t2 * T;
+        double quangDuongB = t2 * 4 * B + 2 * B * Math.Abs(Math.Cos(omegaB * tconlaiB + Math.PI / 2));
+        Console.WriteLine($"Quang Duong vat B di chuyen duoc sau {tB}s la: {quangDuongB}cm\n");
+        
+    }
+}
---
+++ BTVN/BTVNT2/BTVN2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BTVN2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double v0 = 20, goc = 30 * Math.PI / 180, g = 9.8;
+        double v0x = v0 * Math.Cos(goc);
+        double v0y = v0 * Math.Sin(goc);
+        double tMax = v0y / g;
+        double H = v0y * tMax - 0.5 * g * Math.Pow(tMax, 2);
+        double T = 2 * tMax;
+        double qDuong = v0x * T;
+        Console.WriteLine("Van toc ban dau theo phuong ngang: v0x = {0}", v0x);
+        Console.WriteLine("Van toc ban dau theo phuong ngang: v0x = {0}", v0y);
+        Console.WriteLine("Thoi gian vat len den diem cao nhat la: tMax = {0}", tMax);
+        Console.WriteLine("Chieu cao cuc dai ma vat dat duoc la: H = {0}", H);
+        Console.WriteLine("Quang duong ngang ma vat di duoc khi tro lai mat dat la: qDuong = {0}", qDuong);
+    }
+
+}
